031eddfbe0d4d3e812255b155f49a12325e1dbde
Will_tam <will_tam@club-internet.fr>
2017-10-16 00:17:09 +0200

    Begin score table display. Add some comments in console_mode.py

--- console_mode.py
+++ console_mode.py
@@ -38,10 +38,12 @@ def ask_number_of_players():
     question += "1 means 2 players, you and ... me.\n"
     question += "No more than 5 players.\n"
     question += "So >>> "
-    nb_players = 6
+
+    nb_players = 6  # We directely enter inside next loop.
+
     while nb_players > 5:
         try:
-            nb_players = int(input(question))
+            nb_players = int(input(question))   # Only int expeted.
         except:
             print("\n\tSorry, i'm waiting just a number.\n")
 
@@ -79,53 +81,52 @@ def main_loop(playersI, nb_players):
                   nb_players = number of players.
     @return : name of the player who gets away.
     """
-    print("playersI =>", playersI)
-
     game = one_turn.One_Turn()
 
+    # Prepare game's rule according number of player.
     if nb_players == 1:
         some_rules = "I'm great lord, I let you begin {}.\n".format(playersI[0].nickname)
         some_rules += "If you are to scarry, enter 0 now or "
         some_rules += "anytime you want!" + 2 * "\n"
         now_player = 0
-        computer = almostAI.Almost_AI()
+        computer = almostAI.Almost_AI() # If only 1 human, add the computer player.
     else:
         some_rules = "Well, let the random deciding who will begin.\n"
         some_rules += "If one among you is to afraid, enter 0 "
         some_rules += "at your turn.\n"
-        now_player = rnd.randrange(nb_players)
+        now_player = rnd.randrange(nb_players)  # 1st player is a random choice.
         some_rules += "So, the first of you will be {}".format(playersI[now_player].nickname)
         some_rules += 2 * "\n"
 
     print(80*"\n")
     print(some_rules)
 
-    p_answer = ""
+    p_answer = ""   # No player's answer to enter in loop.
 
     while p_answer != "0":
-        if nb_players == 1 and now_player == 1:
+        if nb_players == 1 and now_player == 1:     # Only 1 player, and it's computer's turn.
             p_answer = computer.choice()
             print("My turn >>> {}".format(p_answer))
-        else:
+        else:   # everelse it's player turn (it runs for 1 or several human players).
             print("{} your turn >>>".format(playersI[now_player].nickname), end='')
             p_answer = input(" ")
-            if p_answer == "0":
+            if p_answer == "0":         # 0 means exit game.
                 return playersI[now_player].nickname
 
-        if not game.check_answer(p_answer):
-            if nb_players == 1 and now_player == 1:
+        if not game.check_answer(p_answer):     # bad answer !
+            if nb_players == 1 and now_player == 1:         # Only 1 player and it's the computer's turn.
                 print("\tOooh sh... you win {} !\n".format(playersI[0].nickname))
                 print("\t+1 win point for you, +1 loose point for ... me !\n")
 
-            elif nb_players == 1 and now_player == 0:
+            elif nb_players == 1 and now_player == 0:       # Only 1 player and it's the human's turn.
                 print("\tI win, you loose {}\n".format(playersI[0].nickname))
                 print("\t+1 win point for me, +1 loose point for YOU !\n")
 
-            else:
+            else:       # Several human players.
                 print("\tSorry {}, you loose the turn !\n".format(playersI[now_player].nickname))
                 print("\t+1 loose point for you, +1 win point for the others\n")
 
-            for pI, lostV in enumerate(playersI):
+            for pI, lostV in enumerate(playersI):   # Update win and loose points for each players.
                 if pI == now_player:
                     playersI[pI].loose_rounds += 1
                 else:
@@ -133,11 +134,69 @@ def main_loop(playersI, nb_players):
         else:
             print("\n")
 
-        if nb_players == 1:
-            now_player = xor(now_player, 1)
+        # Go to the next palyer.
+        if nb_players == 1:     # 1 player case.
+            now_player = xor(now_player, 1) # Or player[0] or  player[1] ONLY !!!
         else:
-            now_player = 0 if now_player == nb_players - 1 else now_player + 1
+            now_player = 0 if now_player == nb_players - 1 else now_player + 1  # Several players case.
 
+def display_points(playersI):
+    """
+    Display the win and loose points as a table
+    @parameters : playersI = instances' array of players.
+    @return : none.
+    """
+    won_string = "won rounds"
+    lost_string = "lost rounds"
+
+    # Take the length of the longest nicknames.
+    max_str_len = max([len(player.nickname) for player in playersI])
+
+    # Add the lenth of the 2 strings won_string and lost_string.
+    width = max_str_len + len(won_string) + len(lost_string)
+
+    # Add the border, beetween the 2 strings, beetween nickname and the table body.
+    width += 5
+
+    print(width * "*")
+    print("*{} *{}*{}*".format(max_str_len * " ", won_string, lost_string))
+    for player in playersI:
+        fillspaces = max_str_len - len(player.nickname)
+        print("*{}{} * {} * {} *".format(player.nickname,
+                                         fillspaces * " ",
+                                         player.win_rounds,
+                                         player.loose_rounds))
+
+    print(width * "*", "\n")
+
+    # Search the max of each points.
+    max_win_points = max([player.win_rounds for player in playersI])
+    max_loose_points = max([player.loose_rounds for player in playersI])
+
+    # Pick up the name of each group according the max points of each group.
+    winners = [p.nickname for p in playersI if p.win_rounds == max_win_points]
+    loosers = [p.nickname for p in playersI if p.loose_rounds == max_loose_points]
+
+    # Annoucement, with grammatical correction !!!!
+    if len(winners) > 1:
+        annoucement = "winners are"
+    else:
+        annoucement = "winner is"
+
+    print("And the {} :".format(annoucement))
+    for winner in winners:
+        print("\t{}".format(winner))
+
+    if len(loosers) > 1:
+        annoucement = "loosers are"
+    else:
+        annoucement = "looser is"
+
+    print("So, the {} :".format(annoucement))
+    for looser in loosers:
+        print("\t{}".format(looser))
+
+    print("\n")
 
 def console_mode():
     """
@@ -150,12 +209,15 @@ def console_mode():
     if nb_players == 0:
         print("\nMaybe later !\n")
         return 0
-    nickname_away = main_loop(ask_nickname(nb_players), nb_players)
+    playersI = ask_nickname(nb_players)
+    nickname_away = main_loop(playersI, nb_players)
     if nickname_away:
         if nb_players == 1:
             print("{}Of course, you have feel i was the strongest !\n".format(5 * "\n"))
         else:
-            print("{}{} wants to get away !!!\n".format(5 * "\n", nickname_away))
+            print("{}{} would to get away !!!\n".format(5 * "\n", nickname_away))
+
+    display_points(playersI)
 
     return 0
 
