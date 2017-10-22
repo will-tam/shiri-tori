05f209500eb5cd3e9bced9a08ce5ff26e202bc9e
Will_tam <will_tam@club-internet.fr>
2017-10-22 20:49:33 +0200

    Edict rules

---
+++ rules.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Contains only the game's rules.
+"""
+
+rules = """In this game, a 1st player write a japanese word.
+A second player, write a word beggining with the end of the previous one.
+And so on.
+If a player write a word with a ん or ン ending, he/she looses.
+The forbidden words are :
+    - only one mora words is forbidden (eg: い(胃) - stomach / か　蚊 - mosquito) ;
+    - acronym (eg : ヴィップ - VIP) ;
+    - a ん beggining word (eg : んです) ;
+
+At last, but not least, at this moment, only use hiragana, even for usually words usually writed in katakana.
+"""
